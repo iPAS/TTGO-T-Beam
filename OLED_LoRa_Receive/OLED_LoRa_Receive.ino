63230b8a3453b208b99bbd984add62ce0159c9ec
iPAS <ptiwatthanont@gmail.com>
2021-02-22 23:48:27 +0700

    Add code but not tested.

--- OLED_LoRa_Receive/OLED_LoRa_Receive.ino
+++ OLED_LoRa_Receive/OLED_LoRa_Receive.ino
@@ -69,6 +69,9 @@ void setup() {
     display.flipScreenVertically();
     display.setFont(ArialMT_Plain_10);
 
+    // GPS
+    GPS_Serial1.begin(9600, SERIAL_8N1, 12, 15);   //17-TX 18-RX
+
     delay(1500);
 }
 
@@ -79,8 +82,43 @@ void loop() {
     }
     //   delay(10);
 
+
+Serial.print("Latitude  : ");
+  Serial.println(gps.location.lat(), 5);
+  Serial.print("Longitude : ");
+  Serial.println(gps.location.lng(), 4);
+  Serial.print("Satellites: ");
+  Serial.println(gps.satellites.value());
+  Serial.print("Altitude  : ");
+  Serial.print(gps.altitude.feet() / 3.2808);
+  Serial.println("M");
+  Serial.print("Time      : ");
+  Serial.print(gps.time.hour());
+  Serial.print(":");
+  Serial.print(gps.time.minute());
+  Serial.print(":");
+  Serial.println(gps.time.second());
+  Serial.println("**********************");
+
+    smartDelay(1000);                                      
+
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+    Serial.println(F("No GPS data received: check wiring"));
+
     digitalWrite(14, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(500);              // wait for a second
     digitalWrite(14, LOW);   // turn the LED off by making the voltage LOW
     delay(500);              // wait for a second
+}
+
+
+static void smartDelay(unsigned long ms)                
+{
+  unsigned long start = millis();
+  do
+  {
+    while (GPS_Serial1.available())
+      gps.encode(GPS_Serial1.read());
+  } while (millis() - start < ms);
 }
