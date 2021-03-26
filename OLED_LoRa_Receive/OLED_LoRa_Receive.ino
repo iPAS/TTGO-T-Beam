944a007d427a85a2cb09e890db0261e833cff607
iPAS <ptiwatthanont@gmail.com>
2021-03-26 22:54:29 +0700

    Add auto version cognition in the receiver.

--- OLED_LoRa_Receive/OLED_LoRa_Receive.ino
+++ OLED_LoRa_Receive/OLED_LoRa_Receive.ino
@@ -7,15 +7,8 @@
 #include <axp20x.h>
 
 
-// T-Beam version selection. Differnt version has different hardware design.
-#define TBEAM_V07
-
-
-#ifdef TBEAM_V07
-#define LED_IO 14
-#else
-#define LED_IO 4
-#endif
+//#define BAND    868E6
+#define BAND 923E6
 
 #define LORA_SCK  5   // GPIO5  -- SX1278's SCK
 #define LORA_MISO 19  // GPIO19 -- SX1278's MISO
@@ -24,8 +17,11 @@
 #define LORA_DI0  26  // GPIO26 -- SX1278's IRQ(Interrupt Request)
 #define LORA_RST  23  // GPIO23 -- SX1278's RESET
 
-//#define BAND    868E6
-#define BAND 923E6
+
+#define LED_IO_V07 14
+#define LED_IO_V10 4
+static uint8_t led_io;
+
 
 SSD1306 display(0x3C, 21, 22);
 String  rssi     = "RSSI --";
@@ -33,26 +29,65 @@ String  snr      = "SNR --";
 String  packSize = "--";
 String  packet   = "";
 
+
 #define GPS_BAUDRATE 9600
 
-#ifdef TBEAM_V07
-#define GPS_TX 15
-#define GPS_RX 12
-#else
-#define GPS_TX 12
-#define GPS_RX 34
-#endif
+static uint8_t gps_tx;
+static uint8_t gps_rx;
+
+#define GPS_TX_V07 15
+#define GPS_RX_V07 12
+
+#define GPS_TX_V10 12
+#define GPS_RX_V10 34
 
 TinyGPSPlus gps;
 String gps_datetime = "@ --";
 String gps_loc  = "SAT --, LAT --, LON --, ALT --";
+
 
 AXP20X_Class axp;
 #define AXP_SDA 21
 #define AXP_SCL 22
 #define AXP_IRQ 35
 
-void loraData() {
+// ---------- AXP192 ----------
+void axp_setup() {
+    axp.setLDO2Voltage(3300);   // LoRa VDD
+    axp.setLDO3Voltage(3300);   // GPS  VDD
+    axp.setPowerOutPut(AXP192_LDO2, AXP202_ON);  // LORA radio
+    axp.setPowerOutPut(AXP192_LDO3, AXP202_ON);  // GPS main power
+
+    axp.setDCDC1Voltage(3300);  // for the OLED power
+    axp.setPowerOutPut(AXP192_DCDC1, AXP202_ON);
+
+    axp.setPowerOutPut(AXP192_DCDC2, AXP202_ON);
+    axp.setPowerOutPut(AXP192_EXTEN, AXP202_ON);
+}
+
+// ---------- LoRa ----------
+void lora_setup() {
+    SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_SS);
+    LoRa.setPins(LORA_SS, LORA_RST, LORA_DI0);
+    if (!LoRa.begin(BAND)) {
+        Serial.println("Starting LoRa failed!");
+        while (1);
+    }
+
+    LoRa.setSpreadingFactor(12);  // ranges from 6-12, default 7 see API docs. Changed for ver 0.1 Glacierjay
+    // LoRa.setSignalBandwidth(7.8E3);  // signalBandwidth - signal bandwidth in Hz, defaults to 125E3.
+                                     // Supported values are 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3, 41.7E3, 62.5E3, 125E3, 250E3, and 500E3.
+    // LoRa.setTxPower(20, PA_OUTPUT_PA_BOOST_PIN);  // Set maximum Tx power to 20 dBm (17 is default).
+                                                  // https://github.com/sandeepmistry/arduino-LoRa/blob/master/API.md#tx-power
+    // LoRa.setGain(0);  // Supported values are between 0 and 6. If gain is 0, AGC will be enabled and LNA gain will not be used. 
+                      // Else if gain is from 1 to 6, AGC will be disabled and LNA gain will be used.
+
+    // LoRa.onReceive(cbk);
+    LoRa.receive();
+    Serial.println("LoRa ok");
+}
+
+void lora_data() {
     display.clear();
     display.setTextAlignment(TEXT_ALIGN_LEFT);
     display.setFont(ArialMT_Plain_10);
@@ -76,9 +111,10 @@ void cbk(int packetSize) {
     rssi = "RSSI " + String(LoRa.packetRssi(), DEC);
     snr  = "SNR "  + String(LoRa.packetSnr(), 2);
 
-    loraData();
+    lora_data();
 }
 
+// ---------- Aux functions ----------
 void smartDelay(unsigned long ms) {
     unsigned long start = millis();
     do {
@@ -88,63 +124,56 @@ void smartDelay(unsigned long ms) {
     } while (millis() - start < ms);
 }
 
+// ---------- Setup ----------
 void setup() {
-    pinMode(LED_IO, OUTPUT);
-    digitalWrite(LED_IO, LOW);
-
     Serial.begin(115200);
     while (!Serial);
     Serial.println();
 
-    SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_SS);
-    LoRa.setPins(LORA_SS, LORA_RST, LORA_DI0);
-    if (!LoRa.begin(BAND)) {
-        Serial.println("Starting LoRa failed!");
-        while (1);
-    }
-
-    LoRa.setSpreadingFactor(12);  // ranges from 6-12, default 7 see API docs. Changed for ver 0.1 Glacierjay
-    // LoRa.setSignalBandwidth(7.8E3);  // signalBandwidth - signal bandwidth in Hz, defaults to 125E3.
-                                     // Supported values are 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3, 41.7E3, 62.5E3, 125E3, 250E3, and 500E3.
-    // LoRa.setTxPower(20, PA_OUTPUT_PA_BOOST_PIN);  // Set maximum Tx power to 20 dBm (17 is default).
-                                                  // https://github.com/sandeepmistry/arduino-LoRa/blob/master/API.md#tx-power
-    // LoRa.setGain(0);  // Supported values are between 0 and 6. If gain is 0, AGC will be enabled and LNA gain will not be used. 
-                      // Else if gain is from 1 to 6, AGC will be disabled and LNA gain will be used.
-
-    // LoRa.onReceive(cbk);
-    LoRa.receive();
-    Serial.println("LoRa ok");
     display.init();
     display.flipScreenVertically();
     display.setFont(ArialMT_Plain_10);
 
-    #ifndef TBEAM_V07
+    delay(500);
+
+
+    // Version validation
     Wire.begin(AXP_SDA, AXP_SCL);
     if (axp.begin(Wire, AXP192_SLAVE_ADDRESS) == AXP_FAIL) {
-        Serial.println("Starting AXP192 failed!");
-        while (1);
+        Serial.println("Starting AXP192 failed! -- guessing, this is the V0.7");
+
+        led_io = LED_IO_V07;
+        gps_tx = GPS_TX_V07;
+        gps_rx = GPS_RX_V07;
+
+    } else {
+        Serial.println("Starting AXP192 succeeded! -- guessing, its version >= V1.0");
+
+        axp_setup();
+        led_io = LED_IO_V10;
+        gps_tx = GPS_TX_V10;
+        gps_rx = GPS_RX_V10;
     }
 
-    axp.setLDO2Voltage(3300);   // LoRa VDD
-    axp.setLDO3Voltage(3300);   // GPS  VDD
-    axp.setPowerOutPut(AXP192_LDO2, AXP202_ON);  // LORA radio
-    axp.setPowerOutPut(AXP192_LDO3, AXP202_ON);  // GPS main power
 
-    axp.setDCDC1Voltage(3300);  // for the OLED power
-    axp.setPowerOutPut(AXP192_DCDC1, AXP202_ON);
+    // LED
+    pinMode(led_io, OUTPUT);
+    digitalWrite(led_io, LOW);
 
-    axp.setPowerOutPut(AXP192_DCDC2, AXP202_ON);
-    axp.setPowerOutPut(AXP192_EXTEN, AXP202_ON);
-    #endif
+
+    // LoRa
+    lora_setup();
+
 
     // GPS
-    Serial1.begin(GPS_BAUDRATE, SERIAL_8N1, GPS_RX, GPS_TX);
+    Serial1.begin(GPS_BAUDRATE, SERIAL_8N1, gps_rx, gps_tx);
     while (!Serial1);
     smartDelay(1500);
 }
 
+// ---------- Main ----------
 void loop() {
-    digitalWrite(LED_IO, HIGH);  // turn the LED on (HIGH is the voltage level)
+    digitalWrite(led_io, HIGH);  // turn the LED on (HIGH is the voltage level)
     smartDelay(500);
 
     if (gps.satellites.isValid() && gps.time.isUpdated() && gps.location.isValid()) {
@@ -169,6 +198,6 @@ void loop() {
         cbk(packetSize);
     }
 
-    digitalWrite(LED_IO, LOW);   // turn the LED off by making the voltage LOW
+    digitalWrite(led_io, LOW);   // turn the LED off by making the voltage LOW
     smartDelay(500);
 }
